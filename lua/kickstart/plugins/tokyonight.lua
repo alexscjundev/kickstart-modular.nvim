bb90b60101d5e76beadfd571974f44a5a935572f
Alex Jun <lushcluejug@gmail.com>
2024-05-26 12:40:24 -0700

    try out some colors - still like tokyo night the best.

--- lua/kickstart/plugins/tokyonight.lua
+++ lua/kickstart/plugins/tokyonight.lua
@@ -5,21 +5,31 @@ return {
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'folke/tokyonight.nvim',
+    -- 'twerth/ir_black',
+    -- 'chriskempson/tsquarefrog
+    -- 'squarefrog/tomorrow-night.vim',
+    -- 'projekt0n/github-nvim-theme',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'ir_black'
+      -- vim.cmd.colorscheme 'Tomorrow-night'
+
+      -- vim.cmd 'colorscheme github_dark_dimmed'
+      -- vim.cmd 'colorscheme github_dark_default'
+      -- vim.cmd 'colorscheme github_dark_high_contrast'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
     end,
-    opts = {
-      on_highlights = function(hl, c)
-        hl.WinSeparator = { fg = c.purple }
-      end,
-    },
+    -- opts = {
+    --   on_highlights = function(hl, c)
+    --     hl.WinSeparator = { fg = c.purple }
+    --   end,
+    -- },
   },
 }
 -- vim: ts=2 sts=2 sw=2 et
