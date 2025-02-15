e7956faa09983e0fbe0d29dd4baf7adc7d16f192
alexscjundev <lushcluejug@gmail.com>
2025-02-14 23:48:01 -0500

    honestly not sure what this is?

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -44,15 +44,15 @@ return {
     { '<leader>x', ':Neotree close<CR>', desc = 'Close NeoTree', silent = true },
   },
   opts = {
-    close_if_last_window = false,  -- Prevents auto-closing
-    popup_border_style = "rounded",
+    close_if_last_window = false, -- Prevents auto-closing
+    popup_border_style = 'rounded',
     enable_git_status = true,
     enable_diagnostics = true,
-    
+
     filesystem = {
       follow_current_file = {
-        enabled = true,          -- Focus the current file in tree
-        leave_dirs_open = true,  -- Keep parent directories open
+        enabled = true, -- Focus the current file in tree
+        leave_dirs_open = true, -- Keep parent directories open
       },
       filtered_items = {
         visible = false,
@@ -60,32 +60,36 @@ return {
         hide_gitignored = false,
         hide_hidden = false,
       },
-      hijack_netrw_behavior = "open_default",
+      hijack_netrw_behavior = 'open_default',
       use_libuv_file_watcher = true,
     },
 
     window = {
-      position = "left",
+      position = 'left',
       width = 30,
       mappings = {
         ['\\'] = 'close_window',
-        ['e'] = function() vim.api.nvim_exec('Neotree focus filesystem left', true) end,
-        ['b'] = function() vim.api.nvim_exec('Neotree focus buffers left', true) end,
-        ['g'] = function() vim.api.nvim_exec('Neotree focus git_status left', true) end,
-        ["<space>"] = "toggle_node",
-        ["<2-LeftMouse>"] = "open",
-        ["<cr>"] = "open",
-        ["l"] = "open",
-        ["h"] = "close_node",
-        ["a"] = "add",
-        ["d"] = "delete",
-        ["r"] = "rename",
-        ["c"] = "copy",
-        ["m"] = "move",
-        ["q"] = "close_window",
-        ["R"] = "refresh",
-        ["<C-b>"] = function()
-          vim.cmd("wincmd p")  -- Jump to previous (last accessed) window
+        ['e'] = function()
+          vim.api.nvim_exec('Neotree focus filesystem left', true)
+        end,
+        ['b'] = function()
+          vim.api.nvim_exec('Neotree focus buffers left', true)
+        end,
+        -- ['g'] = function() vim.api.nvim_exec('Neotree focus git_status left', true) end,
+        ['<space>'] = 'toggle_node',
+        ['<2-LeftMouse>'] = 'open',
+        ['<cr>'] = 'open',
+        ['l'] = 'open',
+        ['h'] = 'close_node',
+        ['a'] = 'add',
+        ['d'] = 'delete',
+        ['r'] = 'rename',
+        ['c'] = 'copy',
+        ['m'] = 'move',
+        ['q'] = 'close_window',
+        ['R'] = 'refresh',
+        ['<C-b>'] = function()
+          vim.cmd 'wincmd p' -- Jump to previous (last accessed) window
         end,
       },
     },
@@ -95,15 +99,15 @@ return {
         indent_size = 2,
         padding = 1,
         with_markers = true,
-        indent_marker = "│",
-        last_indent_marker = "└",
-        highlight = "NeoTreeIndentMarker",
+        indent_marker = '│',
+        last_indent_marker = '└',
+        highlight = 'NeoTreeIndentMarker',
       },
       icon = {
-        folder_closed = "",
-        folder_open = "",
-        folder_empty = "",
-        default = "",
+        folder_closed = '',
+        folder_open = '',
+        folder_empty = '',
+        default = '',
       },
       name = {
         trailing_slash = false,
@@ -111,17 +115,18 @@ return {
       },
       git_status = {
         symbols = {
-          added     = "✚",
-          modified  = "",
-          deleted   = "✖",
-          renamed   = "󰁕",
-          untracked = "",
-          ignored   = "",
-          unstaged  = "󰄱",
-          staged    = "",
-          conflict  = "",
-        }
+          added = '✚',
+          modified = '',
+          deleted = '✖',
+          renamed = '󰁕',
+          untracked = '',
+          ignored = '',
+          unstaged = '󰄱',
+          staged = '',
+          conflict = '',
+        },
       },
     },
   },
 }
+
