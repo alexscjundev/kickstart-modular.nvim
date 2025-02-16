8beef59394e89b97e42df92526a15d965e95ecc8
alexscjundev <lushcluejug@gmail.com>
2025-02-15 23:52:33 -0500

    add moonfly colors - consider removing

--- lua/kickstart/plugins/colorschemes.lua
+++ lua/kickstart/plugins/colorschemes.lua
@@ -26,5 +26,8 @@ return {
       vim.cmd 'set bg=dark'
     end,
   },
+
+  -- This one shows error markers for some reason?
+  -- { 'bluz71/vim-moonfly-colors', name = 'moonfly', lazy = false, priority = 1000 },
 }
 -- vim: ts=2 sts=2 sw=2 et
