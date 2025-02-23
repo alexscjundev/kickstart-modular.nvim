0888f1a3f5364d8946a96c8a5a4ee9beac8c2b15
alexscjundev <lushcluejug@gmail.com>
2025-02-23 17:30:50 -0500

    force vscode theme

--- lua/kickstart/plugins/colorschemes.lua
+++ lua/kickstart/plugins/colorschemes.lua
@@ -22,7 +22,7 @@ return {
     'EdenEast/nightfox.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
-      vim.cmd.colorscheme 'carbonfox'
+      -- vim.cmd.colorscheme 'carbonfox'
       vim.cmd 'set bg=dark'
     end,
   },
